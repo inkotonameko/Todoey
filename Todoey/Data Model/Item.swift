b6463c65d82d2c47272e94d433701cdf02e20099
林晓圳 <kimero52yy@vip.qq.com>
2019-02-25 17:35:15 +0800

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by euphoria on 2019/2/25.
+//  Copyright © 2019 maudlin. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title : String = ""
+    var done : Bool = false
+}
